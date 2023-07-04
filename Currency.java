4bdd207f80ef59d51889f030e0c99104f52b1051
mrnagendra8 <132138087+mrnagendra8@users.noreply.github.com>
2023-07-04 17:16:04 +0530

    Add files via upload

---
+++ Currency.java
@@ -0,0 +1,37 @@
+package Currency;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Currency {
+
+	public static void main(String[] args) {
+		//int numberofNotes=Integer.parseInt(args[0]);
+		//System.out.println(numberofNotes);
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter Number of Notesin your country");
+		int numberOfNotes=sc.nextInt();
+		
+		int currency[]=new int[numberOfNotes];
+		
+		System.out.println("Enter the " + numberOfNotes + "denomination values in any order");
+		for(int i = 0; i < currency.length; i++) {
+			currency[i]=sc.nextInT();
+			
+		}
+		
+		System.out.println("Before Sorting" +Arrays.toString(currency));
+		
+		MergeSort ms=new MergeSort();
+		ms.sort(currency, 0, currency.length-1);
+		
+		System.out.println("After Sorting" +Arrays.toString(currency));
+		int amount=sc.nextInt();
+		
+		NoteCount nc=new NoteCount();
+		nc.counting(currency,amount);
+	}
+
+}
